137ee55845a40ca088fa11267633a2a7b2716cfd
alessandrovaru <alessandrovaru@gmail.com>
2022-08-18 12:07:30 -0400

    Connecting to Notion API db

--- app/controllers/notion_controller.rb
+++ app/controllers/notion_controller.rb
@@ -1,7 +1,13 @@
 class NotionController < ApplicationController
   def hello
     @message = 'Hello World!'
-    
     render json: { message: @message }
   end
+  
+  def query_db
+    client = Notion::Client.new
+    @data = client.database_query(database_id: ENV['NOTION_DB_ID'])
+    render json: @data
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root to: "notion#hello"
+  root to: "notion#query_db"
 end
