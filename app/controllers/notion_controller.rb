afa83b3e05e9e42f8df934117aa4712c221b1e08
alessandrovaru <alessandrovaru@gmail.com>
2022-08-18 20:26:08 -0400

    Adding a new method that retrieves images

--- app/controllers/notion_controller.rb
+++ app/controllers/notion_controller.rb
@@ -11,17 +11,28 @@ class NotionController < ApplicationController
   end
 
   
-  def all_pages
+  def porfolio_load
     client = Notion::Client.new
-    data = client.database_query(database_id: ENV['NOTION_DB_ID'])
-    @list_of_pages = data.results
+    data = client.database_query(database_id: ENV['NOTION_PORFOLIO_DB_ID'])
+    @names = data.results
     @page_names = []
-    @list_of_pages.each do |page|
+    @names.each do |page|
       page_content = client.page(page_id: page.id)
       @page_names << page_content.properties.framework.title[0].text.content
     end 
     render json: @page_names
-    
+  end
+
+  def logo_load
+    client = Notion::Client.new
+    data = client.database_query(database_id: ENV['NOTION_LOGOS_DB_ID'])
+    @names = data.results
+    @page_names = []
+    @names.each do |page|
+      page_content = client.page(page_id: page.id)
+      @page_names << page_content.properties.image.files[0].file.url
+    end 
+    render json: @page_names
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root to: "notion#all_pages"
+  root to: "notion#logo_load"
 end
